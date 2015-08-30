c11d342d6c12986756096d7f8bac3447a3a35189
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-08-30 20:50:43 +0300

    v1.0.5
       - Add support for `RouteController`
       - Better, rewritten code in CoffeeScript as a Class
       - Support for dynamic CSS
       - Add "demo" app

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: 'ostrio:iron-router-meta',
-  version: '1.0.4',
-  summary: 'Change meta tags on the fly within iron-router',
+  version: '1.0.5',
+  summary: 'Change meta tags, links and CSS on the fly within iron-router',
   git: 'https://github.com/VeliovGroup/Meteor-iron-router-meta',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['underscore', 'jquery', 'iron:router@1.0.7'], 'client');
-  api.imply('ostrio:iron-router-title@0.2.2');
-  api.addFiles('iron-router-meta.js', 'client');
+  api.use(['underscore', 'jquery', 'iron:router@1.0.7', 'coffeescript', 'ostrio:iron-router-helper-class@1.0.0'], 'client');
+  api.addFiles('iron-router-meta.coffee', 'client');
+  api.imply('ostrio:iron-router-helper-class@1.0.0');
+  api.imply('ostrio:iron-router-title@1.0.0');
 });
